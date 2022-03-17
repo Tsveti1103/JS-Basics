3aa8c621ce43e8d57e0b542bbdf6cd660bab6df5
Tsveti1103 <cvstoqnova@gmail.com>
2022-03-17 14:48:46 +0200

    Create evenOrOdd.js

---
+++ JS basics/conditionalStatementsLab/evenOrOdd.js
@@ -0,0 +1,9 @@
+function evenOrOdd(input) {
+    let obtainedNumber = Number(input[0]);
+    if (obtainedNumber % 2 === 0) {
+        console.log("even");
+    }
+    else {
+        console.log("odd");
+    }
+}
