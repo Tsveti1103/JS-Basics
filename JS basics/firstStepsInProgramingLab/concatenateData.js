bd7bb55e04fd6a4e29f7d79447de274d624bd44b
Tsveti1103 <cvstoqnova@gmail.com>
2022-03-22 14:49:44 +0200

    Create concatenateData.js

---
+++ JS basics/firstStepsInProgramingLab/concatenateData.js
@@ -0,0 +1,8 @@
+function concatenate(input){
+    let firstName = input[0];
+    let lastName = input[1];
+    let age = input[2];
+    let town = input[3];
+    console.log(`You are ${firstName} ${lastName}, a ${age}-years old person from ${town}.`)
+}
+concatenate(["Maria", "Ivanova", 20, "Sofia"])
