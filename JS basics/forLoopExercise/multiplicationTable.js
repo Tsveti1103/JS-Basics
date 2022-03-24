2d546dc189f9008bd4930f5ce2f7eaf22d0f127d
Tsveti1103 <cvstoqnova@gmail.com>
2022-03-24 14:50:17 +0200

    Create multiplicationTable.js

---
+++ JS basics/forLoopExercise/multiplicationTable.js
@@ -0,0 +1,8 @@
+function multiplicationTable(input){
+    let n = Number(input[0]);
+    for(let i =1;i<=10;i++){
+        let result = n*i;
+        console.log(`${i} * ${n} = ${result}`);
+    }
+}
+multiplicationTable(["7"])
