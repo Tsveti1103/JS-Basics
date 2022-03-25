ed2c2c31236768d07d47e5eaaacd4cdc5174718c
Tsveti1103 <cvstoqnova@gmail.com>
2022-03-25 14:50:26 +0200

    Create characterSequence.js

---
+++ JS basics/forLoopLab/characterSequence.js
@@ -0,0 +1,9 @@
+function characterSequence(input) {
+    let text = input[0];
+    let inputLength = text.length;
+    for (let index = 0; index < inputLength; index++) {
+        console.log(text[index]);
+
+    }
+}
+characterSequence(["softuni"])
